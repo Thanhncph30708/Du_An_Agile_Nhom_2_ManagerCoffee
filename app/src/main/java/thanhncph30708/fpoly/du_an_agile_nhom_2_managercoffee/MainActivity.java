0aa02ef5c4cbe3c465f7e5c1e06a82ef0da4b9d4
thanh <thanhncph30708@fpt.edu.vn>
2024-01-19 13:07:38 +0700

    Initial commit

---
+++ app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/MainActivity.java
@@ -0,0 +1,14 @@
+package thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
