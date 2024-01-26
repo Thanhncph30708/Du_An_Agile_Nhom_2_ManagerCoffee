76f045b668ce55eedabbb2136175b3d45db51356
Sam-Trung-Pham <samtrung0809@gmail.com>
2024-01-26 14:32:36 +0700

    code giao diện menu và trang chủ

---
+++ app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/HomeFragment.java
@@ -0,0 +1,44 @@
+package thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main2, container, false);
+
+        return view;
+    }
+
+}
