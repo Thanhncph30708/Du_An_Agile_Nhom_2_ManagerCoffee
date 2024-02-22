9847aebc5d0118a2f18876676ceb453305c18d0b
thanh <thanhncph30708@fpt.edu.vn>
2024-02-22 22:34:44 +0700

    Doi mat khau

--- app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/DoiMatKhauFragment.java
+++ app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/DoiMatKhauFragment.java
@@ -9,25 +9,108 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DAO.NhanVienDAO;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DTO.NhanVien;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.MainActivity;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.ManHinhDangNhap;
 import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.R;
 
 
 public class DoiMatKhauFragment extends Fragment {
 
 
+    MainActivity mainActivity;
+    EditText edMatKhauCu, edMatKhauMoi, edNhapLaiMatKhau;
+    Button btnLuu, btnHuy;
+    NhanVienDAO dao;
+
     @SuppressLint("MissingInflatedId")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_doi_mat_khau, container, false);
+        edMatKhauCu = view.findViewById(R.id.edMatKhauCu);
+        edMatKhauMoi = view.findViewById(R.id.edMatKhauMoi);
+        edNhapLaiMatKhau = view.findViewById(R.id.edNhapLaiMatKhau);
+        btnLuu = view.findViewById(R.id.btnLuyDMK);
+        btnHuy = view.findViewById(R.id.btnHuyDMK);
 
+        mainActivity = (MainActivity) getActivity();
+
+        dao = new NhanVienDAO(getActivity());
+
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edMatKhauCu.setText("");
+                edMatKhauMoi.setText("");
+                edNhapLaiMatKhau.setText("");
+            }
+        });
+
+        btnLuu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences pref = getActivity().getSharedPreferences("USER_FILE", Context.MODE_PRIVATE);
+                String tenDangNhap = pref.getString("SDT", "");
+                if(validate() >0){
+                    dao = new NhanVienDAO(getContext());
+                    NhanVien obj = dao.getSDT(tenDangNhap);
+                    obj.setMatKhau(edMatKhauMoi.getText().toString());
+                    if(dao.updateNhanVien(obj)>0){
+                        Toast.makeText(getActivity(), "Thay đổi mật khấu thành công", Toast.LENGTH_SHORT).show();
+                        edMatKhauCu.setText("");
+                        edMatKhauMoi.setText("");
+                        edNhapLaiMatKhau.setText("");
+                        Intent intent = new Intent(getActivity(), ManHinhDangNhap.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(getActivity(), "Thay đổi mật khẩu không thành công", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
         return view;
+    }
+
+    private int validate(){
+        int check = 1;
+        if(edMatKhauCu.getText().length()==0 ||
+                edMatKhauMoi.getText().length()==0 ||
+                edNhapLaiMatKhau.getText().length()==0){
+            Toast.makeText(getContext(), "Bạn phải nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+            check = -1;
+        }else{
+            SharedPreferences pref = getActivity().getSharedPreferences("USER_FILE",Context.MODE_PRIVATE);
+            String matKhauCu = pref.getString("PASSWORD", "");
+            String matKhauMoi = edMatKhauMoi.getText().toString();
+            String nhapLaiMatKhau = edNhapLaiMatKhau.getText().toString();
+            if (!edMatKhauCu.getText().toString().equals(matKhauCu)){
+                edMatKhauCu.setError("Mật khẩu cũ sai");
+                check = -1;
+            }else{
+                edMatKhauCu.setError(null);
+                if (!matKhauMoi.equals(nhapLaiMatKhau)){
+                    edNhapLaiMatKhau.setError("Mật khẩu không trùng khớp");
+                    check = -1;
+                }else{
+                    edMatKhauCu.setError(null);
+                    edMatKhauMoi.setError(null);
+                    edNhapLaiMatKhau.setError(null);
+                }
+            }
+
+
+        }
+        return check;
     }
 
 
