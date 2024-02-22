4ace923e5a8805ac91b4adedc4370dcd5687b5c3
thanh <thanhncph30708@fpt.edu.vn>
2024-02-22 23:01:01 +0700

    Bieu do

--- app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/BieuDoFragment.java
+++ app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/BieuDoFragment.java
@@ -9,19 +9,68 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import java.util.ArrayList;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DAO.ThongKeDAO;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DTO.DoanhThu;
 import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.R;
 
 public class BieuDoFragment extends Fragment  {
 
 
 
+    LineChart lineChart;
+    ArrayList<DoanhThu> list;
+    @Nullable
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_bieu_do_fragment, container, false);
+        lineChart = view.findViewById(R.id.lineChart);
 
+        LineDataSet lineDataSet = new LineDataSet(dataValues(),"Doanh thu");
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(lineDataSet);
+        LineData data = new LineData(lineDataSet);
+        lineChart.setData(data);
+
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getXAxis().setGranularity(1.0f);
+
+        lineDataSet.setColors( Color.RED);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(18f);
+
+        lineChart.invalidate(); //refesh
         return view;
     }
 
+    private List<Entry> dataValues() {
+        ThongKeDAO thongKeDAO = new ThongKeDAO(getContext());
+        list = (ArrayList<DoanhThu>) thongKeDAO.getBieuDo();
 
+        List<Entry> dataValue = new ArrayList<>();
+        dataValue.add(new Entry(0, 0));
+
+        for (int i = 0; i < list.size(); i++) {
+            int thang = list.get(i).getThang();
+            int tongTien = list.get(i).getTongTien();
+            dataValue.add(new Entry(list.get(i).getThang(), list.get(i).getTongTien()));
+            Log.d("zzzzz", "dataValues: " + thang + " tổng: " + tongTien);
+        }
+
+
+        return dataValue;
+
+    }
 }
