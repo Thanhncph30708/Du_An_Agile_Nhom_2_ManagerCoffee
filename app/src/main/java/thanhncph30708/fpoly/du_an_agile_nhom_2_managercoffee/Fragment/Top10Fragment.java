591fb8abd8c1831f878a5eb6a2de1e6e0ee26ad0
thanh <thanhncph30708@fpt.edu.vn>
2024-02-22 22:42:43 +0700

    Top 10

--- app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/Top10Fragment.java
+++ app/src/main/java/thanhncph30708/fpoly/du_an_agile_nhom_2_managercoffee/Fragment/Top10Fragment.java
@@ -12,16 +12,32 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.Adapter.AdapterTop10;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DAO.ThongKeDAO;
+import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.DTO.Top10;
 import thanhncph30708.fpoly.du_an_agile_nhom_2_managercoffee.R;
 
 
 public class Top10Fragment extends Fragment {
 
 
+    ListView lv;
+    ArrayList<Top10> list;
+    AdapterTop10 adapterTop10;
+    ThongKeDAO dao;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_top_10,container,false);
+        lv = view.findViewById(R.id.lvTop);
+        dao  = new ThongKeDAO(getActivity());
+        list = (ArrayList<Top10>) dao.getTop();
+        adapterTop10 = new AdapterTop10(getActivity(), this, list);
+        lv.setAdapter(adapterTop10);
+
+
+
         return  view;
 
     }
